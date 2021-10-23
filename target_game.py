12dec9c5ca4341e0d22fb44d9ce73ec618387986
Tetiana Vinnik <tetiana.vinnik@ucu.edu.ua>
2021-10-23 20:38:58 +0300

    Create functions get_pure_user_words and results

--- target_game.py
+++ target_game.py
@@ -24,8 +24,6 @@ def generate_grid() -> List[List[str]]:
     print(playground)
     return list_of_letters
 
-#generate_grid()
-
 
 def get_words(f: str, letters: List[str]) -> List[str]:
     """
@@ -46,8 +44,8 @@ def get_words(f: str, letters: List[str]) -> List[str]:
 
     possible_words = []
     with open(f, 'r') as file:
-        for i in file:
-            line = file.readline()
+        file1 = file.readlines()
+        for line in file1:
             if '\n' in line:
                 line = line.replace('\n', '')
             count = 0
@@ -60,7 +58,6 @@ def get_words(f: str, letters: List[str]) -> List[str]:
                 count += 1
             if count == 0:
                 possible_words.append(line)
-        #print(possible_words)
 
         possible_words_list = possible_words.copy()
         for word in possible_words:
@@ -91,19 +88,16 @@ def get_words(f: str, letters: List[str]) -> List[str]:
                     break
             for h in range(len(grid_tpl_lst)):
                 if grid_tpl_lst[h][1] < letters_in_line_tpl[h][1]:
-                    possible_words_list.remove(word)
-    #print(possible_words_list)
+                    if word in possible_words_list:
+                        possible_words_list.remove(word)
     return possible_words_list
 
-
-#get_words('en.txt', generate_grid())
 
 def get_user_words() -> List[str]:
     """
     Gets words from user input and returns a list with these words.
     Usage: enter a word or press ctrl+d to finish.
     """
-    letters = generate_grid()
     try:
         user_list = []
         m = 0
@@ -135,7 +129,8 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
             else:
                 for k in n:
                     if k not in letters:
-                        pure_lst.remove(n)
+                        if n in pure_lst:
+                            pure_lst.remove(n)
 
     grid_tpl_lst = []
     letters = sorted(letters)
@@ -165,25 +160,48 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
                     letters_in_line_tpl.append(tpl_line)
                     lst_line = sorted(lst_line)
 
-    lst_line_copy = lst_line.copy()
-    letters_copy = letters.copy()
-    for r in letters_copy:
-        if r not in lst_line_copy:
-            letters_in_line_tpl.append((r, 0))
-    letters_in_line_tpl = sorted(letters_in_line_tpl, key=operator.itemgetter(0))        
-    t = 0
-    while t != len(letters_in_line_tpl)-2:
-        if letters_in_line_tpl[t] == letters_in_line_tpl[t+1]:
-            del letters_in_line_tpl[t]
-        t += 1
-        if t == len(letters_in_line_tpl)-2:
-            break
-    for h in range(len(grid_tpl_lst)):
-        if grid_tpl_lst[h][1] < letters_in_line_tpl[h][1]:
-            possible_words_list.remove(word)
+        for r in letters_copy:
+            if r not in lst_line_copy:
+                letters_in_line_tpl.append((r, 0))
+        letters_in_line_tpl = sorted(letters_in_line_tpl, key=operator.itemgetter(0))        
+        t = 0
+        while t != len(letters_in_line_tpl)-2:
+            if letters_in_line_tpl[t] == letters_in_line_tpl[t+1]:
+                del letters_in_line_tpl[t]
+            t += 1
+            if t == len(letters_in_line_tpl)-2:
+                break
+        for h in range(len(grid_tpl_lst)):
+            if grid_tpl_lst[h][1] < letters_in_line_tpl[h][1]:
+                if word in possible_words_list:
+                    possible_words_list.remove(word)
 
-    print(possible_words_list)
     return possible_words_list
 
+
 def results():
+    letters = generate_grid()
+    dictionary_words = get_words("en.txt", letters)
+    user_words = get_user_words()
+    not_dictionary_words = get_pure_user_words(user_words, letters, dictionary_words)
+    missing_words = []
+    for p in dictionary_words:
+        if p not in user_words:
+            missing_words.append(p)
+    right_words = []
+    for p in dictionary_words:
+        if p in user_words:
+            right_words.append(p)
+    points = len(right_words)
+    with open('result.txt', 'w') as output_file:
+        output_file.write(str(points) + '\n')
+        for i in missing_words:
+            output_file.write(i + '\n')
+        for n in not_dictionary_words:
+            output_file.write(n + '\n')
     
+    print(points)
+    print(missing_words)
+    print(not_dictionary_words)
+
+results()
